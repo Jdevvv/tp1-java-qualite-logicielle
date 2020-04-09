e42cf17edc21337514a6978081f2c27e17636c9c
Jérémy Llanta <jeremy.llanta@gmail.com>
2020-04-09 15:17:50 +0200

    Product & User controller

--- src/main/java/com/tactfactory/monprojetsb/controllers/ProductController.java
+++ src/main/java/com/tactfactory/monprojetsb/controllers/ProductController.java
@@ -1,5 +1,58 @@
 package com.tactfactory.monprojetsb.controllers;
 
-public class ProductController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tactfactory.monprojetsb.entities.Product;
+import com.tactfactory.monprojetsb.repositories.ProductRepository;
+
+
+@Controller
+@RequestMapping(value="products")
+public class ProductController {
+	@Autowired
+	private ProductRepository repository;
+	
+	@RequestMapping(value = {"/index", "/"})
+	 public String index(Model model) {
+		 model.addAttribute("page", "Product index");
+		 model.addAttribute("items", repository.findAll());
+	
+		 return "product/index";
+	 }
+	
+	@GetMapping(value = {"/create"})
+	public String createGet(Model model) {
+		model.addAttribute("page", "Product create");
+		return"product/create";  
+	}
+	
+	@PostMapping(value = {"/create"})
+	public String createPost(@ModelAttribute("product") Product product) {
+		Product newProduct = repository.save(product);
+		
+		return "redirect:product/details/" + String.valueOf(newProduct.getId());  
+	}
+	
+	@GetMapping("/details/{id}")
+    public String details(@ModelAttribute Product product, Model model) {
+		model.addAttribute("page", "Product details");
+        model.addAttribute("item", repository.findById(product.getId()).get());
+        
+        return "product/details";
+    }
+	
+	@DeleteMapping("/delete")
+	public String delete(@ModelAttribute Product product, Model model) {
+		model.addAttribute("page", "Product delete");
+        repository.deleteById(product.getId());
+        
+        return "product/delete";
+	}
 }
--- src/main/java/com/tactfactory/monprojetsb/controllers/UserController.java
+++ src/main/java/com/tactfactory/monprojetsb/controllers/UserController.java
@@ -1,5 +1,48 @@
 package com.tactfactory.monprojetsb.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tactfactory.monprojetsb.entities.User;
+import com.tactfactory.monprojetsb.repositories.UserRepository;
+
+@Controller
+@RequestMapping(value="users")
 public class UserController {
 
+	@Autowired
+	private UserRepository repository;
+	
+	 @RequestMapping(value = {"/index", "/"})
+	 public String index(Model model) {
+		 model.addAttribute("page", "User index");
+		 model.addAttribute("items", repository.findAll());
+	
+		 return "user/index";
+	 }
+	
+	 @GetMapping(value = {"/create"})
+	 public String createGet(Model model) {
+		 model.addAttribute("page", "User create");
+		 return"user/create";
+	 }
+
+	 @PostMapping(value={"/create"})
+	 public String createPost(@ModelAttribute User user) {
+		 if (user != null) {
+			 repository.save(user);
+		 }
+		 return "redirect:index";
+	 }
 }
