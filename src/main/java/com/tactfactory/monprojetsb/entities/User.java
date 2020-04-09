727c392719622b4f55a026916e571026add0cceb
Jérémy Llanta <jeremy.llanta@gmail.com>
2020-04-09 15:17:35 +0200

    Product & User entities

--- src/main/java/com/tactfactory/monprojetsb/entities/User.java
+++ src/main/java/com/tactfactory/monprojetsb/entities/User.java
@@ -23,5 +23,33 @@ public class User {
 		this.products = products;
 	}
 	
+	public User() {
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public List<Product> getProducts() {
+		return products;
+	}
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+	
 	
 }
